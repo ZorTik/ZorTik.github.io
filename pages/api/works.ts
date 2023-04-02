be1e3e71c9b773f3882677fccc3b1a54fca44612
ZorT <67344817+ZorTik@users.noreply.github.com>
2023-04-02 19:36:24 +0200

    works

--- pages/api/works.ts
+++ pages/api/works.ts
@@ -2,8 +2,18 @@ import {NextApiRequest, NextApiResponse} from "next";
 import libxmljs from "libxmljs";
 import * as fs from "fs";
 
-export function loadWorks() {
-    return fs.readdirSync("public/works")
+const localStore = [
+    {
+        title: "Containr GUI",
+        img: "/works/work1/image.png",
+        description: "An ultimate Minecraft GUI framework. This project is part of my public freelance repositories I use in my commissions.",
+        categories: ["Java", "Minecraft", "Library"],
+    },
+]
+
+function loadWorks() {
+    return localStore;
+    /*return fs.readdirSync("public/works")
         .map(file => {
             const meta = fs.readFileSync(`public/works/${file}/meta.xml`, "utf-8");
             const root = libxmljs.parseXml(meta).root();
@@ -12,7 +22,7 @@ export function loadWorks() {
             const description = (root?.find("description") as any)[0].text();
             const categories = root?.find("//category").map((c: any) => c.text());
             return {title, img, description, categories};
-        });
+        });*/
 }
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -26,4 +36,8 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
         });
 
     return res.status(200).json(works);
+}
+
+export {
+    loadWorks,
 }

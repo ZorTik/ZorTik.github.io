ebc7eb906ff0973f2c0886f72be2dcc9dac30e13
ZorT <67344817+ZorTik@users.noreply.github.com>
2023-04-07 18:07:45 +0200

    Update

--- pages/3rdparty.tsx
+++ pages/3rdparty.tsx
@@ -5,6 +5,7 @@ export default function ThirdParty() {
         <BaseLayout>
             <a href="https://www.flaticon.com/free-icons/user" title="user icons">User icons created by Freepik - Flaticon</a><br />
             <a href="https://www.flaticon.com/free-icons/document" title="document icons">Document icons created by vectorsmarket15 - Flaticon</a>
+            <a href="https://www.flaticon.com/free-icons/database" title="database icons">Database icons created by Freepik - Flaticon</a>
         </BaseLayout>
     )
 }

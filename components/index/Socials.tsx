93e87da3cbabd6bd953f962207304b0f8d50dfa3
ZorT <67344817+ZorTik@users.noreply.github.com>
2023-04-26 14:18:33 +0200

    Margin of socials section

--- components/index/Socials.tsx
+++ components/index/Socials.tsx
@@ -3,6 +3,7 @@ import {Button, Col, Row} from "react-bootstrap";
 import {useState} from "react";
 
 const SocialsComponent = styled(Row)`
+  background-color: var(--color-shade-3);
   border: 1px solid var(--color-shade-2);
   margin: 20px 0 80px 0;
   > .col {
@@ -27,7 +28,7 @@ export default function Socials() {
         setDiscordClicked(true);
     }
     return <>
-        <h1 className="text-center mt-5">Let&rsquo;s build something.</h1>
+        <h1 className="text-center" style={{marginTop: "80px"}}>Let&rsquo;s build something.</h1>
         <SocialsComponent className="px-3 py-4" id="contacts">
             <Col><SocialButton bgColor="#4d5fa7" onClick={handleDiscordClick}>{discordClicked ? "Discord tag copied!" : "Discord"}</SocialButton></Col>
             <Col><SocialButton bgColor="#696969" href="https://github.com/ZorTik" target="_blank">GitHub</SocialButton></Col>
